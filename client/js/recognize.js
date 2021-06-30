af06071f202aeddfd4263d85861407d112e407d4
Anshul Gupta <54749519+anshul0365@users.noreply.github.com>
2021-06-30 22:55:08 +0530

    Backend service completed

--- client/js/recognize.js
+++ client/js/recognize.js
@@ -1,16 +1,36 @@
-// $(document).ready(function () {
-//     console.log("on ready")
-//     $.ajax({
-//         type: "POST",
-//         url: "http://127.0.0.1:5000/recognize",
-//         headers: {
-//             'Access-Control-Allow-Origin': '*'
-//         },
-//         success: function (data) {
-//             console.log(data);
-//         },
-//         error: function (error) {
-//             console.error(error)
-//         }
-//     });
-// })
+$(document).ready(function () {
+    console.log("on ready")
+    $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:5000/liveStream",
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function (data) {
+            console.log("success: "+ data)
+            document.getElementById("socketIMG").setAttribute('src', data)
+        },
+        error: function (error) {
+            console.error(error)
+        }
+    });
+})
+
+window.addEventListener("beforeunload", function (e) {
+    // var confirmationMessage = "\o/";
+    e.preventDefault()
+    console.log("onbeforeunload")
+    $.ajax({
+        type: "POST",
+        url: "http://127.0.0.1:5000/stopStream",
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        success: function (data) {
+            console.log("success: " + data)
+        },
+        error: function (error) {
+            console.error(error)
+        }
+    });
+});
--- server/script.py
+++ server/script.py
@@ -1,19 +1,18 @@
-from types import MethodType
+from flask import Flask, request, jsonify, render_template, url_for
+from flask_cors import CORS, cross_origin
+from flask.wrappers import Response
+
+import io
+import os
 import cv2
 import time
 import pickle
-from flask.wrappers import Response
+import base64
 import imutils
 import argparse
+from PIL import Image
 import face_recognition
 from imutils.video import VideoStream
-from flask import Flask, request, redirect, jsonify, json, make_response
-from flask_cors import CORS, cross_origin
-
-import os
-import base64
-from PIL import Image
-import io
 
 app = Flask(__name__)
 CORS(app)
@@ -21,8 +20,12 @@ app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = './dataset'
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return url_for("../client/index.html")
+
+@app.route('/recognize')
+def recognize():
+    return render_template('recognize.html')
 
 @app.route('/saveImage', methods=['POST'])
 @cross_origin()
@@ -93,9 +96,10 @@ def getName():
         return jsonify(statusText="fail", status=400, message="Directory not present. Try again later")
         # return redirect('../index.htm')
 
+# Global Variables
+vs = None #VideoStream Object
+writer = None # Initializing writer varibale to NONE
 
-@app.route('/recognize', methods=['POST'])
-@cross_origin()
 def recognize():
 
     # USAGE
@@ -103,154 +107,143 @@ def recognize():
     # python recognize_faces_video.py --encodings encodings.pickle --output output/jurassic_park_trailer_output.avi --display 0
 
     # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-e", "--encodings", default="encodings.pickle", help="path to serialized db of facial encodings")
+    ap.add_argument("-o", "--output", type=str, help="path to output video")
+    ap.add_argument("-y", "--display", type=int, default=1, help="whether or not to display output frame to screen")
+    ap.add_argument("-d", "--detection-method", type=str, default="hog", help="face detection model to use: either `hog` or `cnn`")
+    args = vars(ap.parse_args())
 
-    def gen():
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-e", "--encodings", default="encodings.pickle", help="path to serialized db of facial encodings")
-        ap.add_argument("-o", "--output", type=str, help="path to output video")
-        ap.add_argument("-y", "--display", type=int, default=1, help="whether or not to display output frame to screen")
-        ap.add_argument("-d", "--detection-method", type=str, default="hog", help="face detection model to use: either `hog` or `cnn`")
-        args = vars(ap.parse_args())
+    # load the known faces and embeddings
+    print("[INFO] loading encodings...")
+    data = pickle.loads(open(args["encodings"], "rb").read())
 
-        # load the known faces and embeddings
-        print("[INFO] loading encodings...")
-        data = pickle.loads(open(args["encodings"], "rb").read())
+    # initialize the video stream and pointer to output video file, then
+    # allow the camera sensor to warm up
+    print("[INFO] starting video stream...")
+    # vs = VideoStream(src=0).start()
+    global vs
+    vs = VideoStream(src=0).start()
+    global writer
+    # writer = None
+    time.sleep(2.0)
 
-        # initialize the video stream and pointer to output video file, then
-        # allow the camera sensor to warm up
-        print("[INFO] starting video stream...")
-        vs = VideoStream(src=0).start()
-        writer = None
-        time.sleep(2.0)
+    # loop over frames from the video file stream
+    while True:
+        # grab the frame from the threaded video stream
+        frame = vs.read()
 
-        # loop over frames from the video file stream
-        while True:
-            # grab the frame from the threaded video stream
-            frame = vs.read()
+        # convert the input frame from BGR to RGB then resize it to have a width of 750px (to speedup processing)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        rgb = imutils.resize(frame, width=750)
+        r = frame.shape[1] / float(rgb.shape[1])
 
-            # convert the input frame from BGR to RGB then resize it to have
-            # a width of 750px (to speedup processing)
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            rgb = imutils.resize(frame, width=750)
-            r = frame.shape[1] / float(rgb.shape[1])
+        # detect the (x, y)-coordinates of the bounding boxes
+        # corresponding to each face in the input frame, then compute
+        # the facial embeddings for each face
+        boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
 
-            # detect the (x, y)-coordinates of the bounding boxes
-            # corresponding to each face in the input frame, then compute
-            # the facial embeddings for each face
-            boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
-            encodings = face_recognition.face_encodings(rgb, boxes)
-            names = []
+        # loop over the facial embeddings
+        for encoding in encodings:
+            # attempt to match each face in the input image to our known encodings
+            matches = face_recognition.compare_faces(data["encodings"], encoding)
+            name = "Unknown"
 
-            # loop over the facial embeddings
-            for encoding in encodings:
-                # attempt to match each face in the input image to our known
-                # encodings
-                matches = face_recognition.compare_faces(data["encodings"], encoding)
-                name = "Unknown"
+            # check to see if we have found a match
+            if True in matches:
+                # find the indexes of all matched faces then initialize a
+                # dictionary to count the total number of times each face
+                # was matched
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
 
-                # check to see if we have found a match
-                if True in matches:
-                        # find the indexes of all matched faces then initialize a
-                        # dictionary to count the total number of times each face
-                        # was matched
-                        matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-                        counts = {}
+                # loop over the matched indexes and maintain a count for
+                # each recognized face face
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
 
-                        # loop over the matched indexes and maintain a count for
-                        # each recognized face face
-                        for i in matchedIdxs:
-                            name = data["names"][i]
-                            counts[name] = counts.get(name, 0) + 1
+                # determine the recognized face with the largest number
+                # of votes (note: in the event of an unlikely tie Python
+                # will select first entry in the dictionary)
+                name = max(counts, key=counts.get)
 
-                        # determine the recognized face with the largest number
-                        # of votes (note: in the event of an unlikely tie Python
-                        # will select first entry in the dictionary)
-                        name = max(counts, key=counts.get)
+            # update the list of names
+            names.append(name)
 
-                # update the list of names
-                names.append(name)
+        # loop over the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            # rescale the face coordinates
+            top = int(top * r)
+            right = int(right * r)
+            bottom = int(bottom * r)
+            left = int(left * r)
 
-            # loop over the recognized faces
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                # rescale the face coordinates
-                top = int(top * r)
-                right = int(right * r)
-                bottom = int(bottom * r)
-                left = int(left * r)
+            # draw the predicted face name on the image
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
-                # draw the predicted face name on the image
-                cv2.rectangle(frame, (left, top), (right, bottom),
-                            (0, 255, 0), 2)
-                y = top - 15 if top - 15 > 15 else top + 15
-                cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75, (0, 255, 0), 2)
+        # if the video writer is None *AND* we are supposed to write
+        # the output video to disk initialize the writer
+        if writer is None and args["output"] is not None:
+            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+            writer = cv2.VideoWriter(args["output"], fourcc, 20, (frame.shape[1], frame.shape[0]), True)
 
-            # if the video writer is None *AND* we are supposed to write
-            # the output video to disk initialize the writer
-            if writer is None and args["output"] is not None:
-                fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-                writer = cv2.VideoWriter(args["output"], fourcc, 20,
-                                (frame.shape[1], frame.shape[0]), True)
-
-            # if the writer is not None, write the frame with recognized
-            # faces t odisk
-            if writer is not None:
-                writer.write(frame)
-
-            # check to see if we are supposed to display the output frame to
-            # the screen
-            if args["display"] > 0:
-                cv2.imshow("Frame", frame)
-                ret, buffer = cv2.imencode('.jpg',frame)
-                frame = buffer.tobytes()
-                yield (frame)
-                # print("frame: ",buffer)
-                key = cv2.waitKey(1) & 0xFF
-
-                # if the `q` key was pressed, break from the loop
-                if key == ord("q"):
-                    break
-
-        # do a bit of cleanup
-        cv2.destroyAllWindows()
-        vs.stop()
-
-        # check to see if the video writer point needs to be released
+        # if the writer is not None, write the frame with recognized faces to disk
         if writer is not None:
-            writer.release()
+            writer.write(frame)
 
-    return Response(gen())
-    
+        # check to see if we are supposed to display the output frame to the screen
+        if args["display"] > 0:
+            # cv2.imshow("Frame", frame)
 
+            retval, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            key = cv2.waitKey(1) & 0xFF
 
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
 
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
 
+    # check to see if the video writer point needs to be released
+    if writer is not None:
+        writer.release()
 
+@app.route('/liveStream')
+@cross_origin()
+def liveStream():
+    return Response(recognize(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/stopStream', methods=['POST'])
+@cross_origin()
+def stopStream():
+    try:
+        global vs
+        global writer
+        # vs.VideoStream().release()
+        # vs.camera(0).release()
+        vs.stop()
+        vs.stream.release()
+        cv2.VideoCapture().release
+        if(vs.stream.release()):
+            print('release')
+        writer.release()
+        if(cv2.destroyAllWindows()):
+            print('cv2')
+        
+        return jsonify(statusText="success", status=200, message="Stream Closed")
+    except Exception as e:
+        return jsonify(statusText="fail", status=400, message=str(e))
+        
 
 @app.errorhandler(404)
 def page_not_found(e):
